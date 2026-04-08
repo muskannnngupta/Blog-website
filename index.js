820e6a0d688cff5574b6e51ca8c35034d439ca7c
muskannnngupta <muskangupta5465@gmail.com>
2026-04-09 02:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
 
